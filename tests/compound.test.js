76f715d27ed754946ac7cbd71b6f097e55faefb7
JModi16 <k5rac70j@students.codeinstitute.net>
2025-03-24 15:10:37 +0000

    code from js file removed

---
+++ tests/compound.test.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.getElementById("investmentForm");
+    form.addEventListener("submit", function(event) {
+        event.preventDefault();
+    });
+});
