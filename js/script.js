1f783cf109faa62efc57d407f3abb7eada0148da
JModi16 <k5rac70j@students.codeinstitute.net>
2025-03-31 22:10:06 +0100

    use const to declare variable cagr for calculation

--- js/script.js
+++ js/script.js
@@ -76,4 +76,16 @@ let totalContributions = 0;
     
              // Decrease withdrawal amount by annual decrease rate
             withdrawalAmount -= withdrawalAmount * annualDecrease;
-           
+        }
+
+        // Calculate Compound Annual Growth Rate (CAGR)
+        const cagr = Math.pow(totalAmount / initialInvestment, 1 / years) - 1;
+
+          // Display the results
+    document.getElementById('futureValue').innerHTML = `Future Value: <span class="black">£${totalAmount.toFixed(2)}</span>`;
+    document.getElementById('totalContributions').innerHTML = `Total Contributions: <span class="black">£${totalContributions.toFixed(2)}</span>`;
+    document.getElementById('totalWithdrawals').innerHTML = `Total Withdrawals: <span class="red">£${totalWithdrawals.toFixed(2)}</span>`;
+    document.getElementById('totalInterest').innerHTML = `Total Interest: <span class="blue">£${(totalAmount - initialInvestment - totalContributions + totalWithdrawals).toFixed(2)}</span>`;
+    document.getElementById('cagr').innerHTML = `CAGR: <span class="green">${(cagr * 100).toFixed(2)}%</span>
+    
+    `;
