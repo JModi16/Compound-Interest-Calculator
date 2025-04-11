d0d6c2c61f90c171165fe84c79ad10551d78f552
JModi16 <k5rac70j@students.codeinstitute.net>
2025-04-11 23:51:47 +0100

    added and removed declared variable

--- js/script.js
+++ js/script.js
@@ -96,7 +96,7 @@ document.addEventListener("DOMContentLoaded", function () {
       // Calculate Compound Annual Growth Rate (CAGR)
       let cagr;
       if (i > 0 && (initialInvestment + totalContributions - totalWithdrawals) > 0) {
-          cagr = Math.pow(totalAmount / (initialInvestment + totalContributions - totalWithdrawals), 1 / (i+1)) - 1;
+          cagr = Math.pow(totalAmount / (initialInvestment + totalContributions - totalWithdrawals), 1 / (i + 1)) - 1;
       } else {
           cagr = 0; // set CAGR to 0 if years is 0 to avoid division by zero
       }
