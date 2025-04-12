cf277d30a1ac34d7f83b0e9562fc66649e19f395
JModi16 <k5rac70j@students.codeinstitute.net>
2025-04-12 15:05:50 +0100

    added decribe function to start jest test

--- tests/compound.test.js
+++ tests/compound.test.js
@@ -1,1 +1,5 @@
-
+describe('Compound Interest Calculator', () => {
+    test('should calculate the correct CAGR', () => {
+        const totalAmount = 20000;
+        const initialInvestment = 10000;
+        const years = 5;
