084136b7c1b3233356dfd27045a437feed293644
JModi16 <k5rac70j@students.codeinstitute.net>
2025-04-20 12:59:06 +0100

    change console.log message for withdrawalamount loop

--- js/script.js
+++ js/script.js
@@ -81,7 +81,7 @@ document.addEventListener("DOMContentLoaded", function () {
             for (let k = 0; k < withdrawalFrequency; k++) {
                 if (withdrawalAmount > 0) {
                     if (totalAmount < withdrawalAmount) {
-                    console.log("Withdrawal amount is 0 or not entered.");
+                    console.log("Insufficient funds for withdrawal.");
                     alert("Please enter a valid withdrawal amount greater than 0.");
                     return; // Stop further execution
                 }
